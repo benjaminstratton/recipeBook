628c4f904c50c565da0c3e1c0e2a311ad06344d8
bstrat <benstratton100@gmail.com>
2023-02-23 19:27:58 -0600

    basic layout for routes

---
+++ controllers/dubai.js
@@ -0,0 +1,40 @@
+const express = require(`express`)
+const router = express.Router()
+const Restaurant = require(`../models/restaurantSchema.js`)
+
+// INDEX
+router.get(`/`, (req, res) => {
+    res.send(`Index`)
+})
+
+// NEW
+router.get(`/new`, (req, res) => {
+    res.send(`New`)
+})
+
+// CREATE
+router.post(`/`, (req, res) => {
+    res.redirect(`/restaurantfinder`)
+})
+
+// SHOW
+router.get(`/:id`, (req, res) => {
+    res.send(`Show`)
+})
+
+// EDIT
+router.get(`/:id/edit`, (req, res) => {
+    res.send(`Edit`)
+})
+
+// UPDATE
+router.put(`/:id`, (req, res) => {
+    res.redirect(`/restaurantfinder`)
+})
+
+// DESTROY
+router.delete(`/:id`, (req, res) => {
+    res.redirect(`/restaurantfinder`)
+})
+
+module.exports = router
--- controllers/restaurantFinder.js
+++ controllers/restaurantFinder.js
@@ -1,1 +1,48 @@
-// Router
+const express = require(`express`)
+const router = express.Router()
+const Restaurant = require(`../models/restaurantSchema.js`)
+const seedData = require(`../models/restaurantFinder.js`)
+
+// SEED
+router.get('/seed', (req, res) => {
+    Restaurant.create(seedData, (err, seededData) => {
+        res.send(seededData)
+    })
+})
+
+// INDEX
+router.get(`/`, (req, res) => {
+    res.send(`Index`)
+})
+
+// NEW
+router.get(`/new`, (req, res) => {
+    res.send(`New`)
+})
+
+// CREATE
+router.post(`/`, (req, res) => {
+    res.redirect(`/restaurantfinder`)
+})
+
+// SHOW
+router.get(`/:id`, (req, res) => {
+    res.send(`Show`)
+})
+
+// EDIT
+router.get(`/:id/edit`, (req, res) => {
+    res.send(`Edit`)
+})
+
+// UPDATE
+router.put(`/:id`, (req, res) => {
+    res.redirect(`/restaurantfinder`)
+})
+
+// DESTROY
+router.delete(`/:id`, (req, res) => {
+    res.redirect(`/restaurantfinder`)
+})
+
+module.exports = router
