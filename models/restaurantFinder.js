09324feab361f43ec9cd3d1f2da8efc30e3bcdc8
bstrat <benstratton100@gmail.com>
2023-02-23 18:04:52 -0600

    created data array for Sydney, London, and Dubai

--- models/restaurantFinder.js
+++ models/restaurantFinder.js
@@ -1,18 +1,232 @@
 module.exports = (
     {
-        location: `New York`,
-        restaurant: [
+        city: `Sydney`,
+        restaurants: [
             {
-                name: `NY`,
-
+                name: `Aria Restaurant Sydney`,
+                address: `1 Macquire St, Sydney NSW 2000, Australia`,
+                website: `https://www.ariasydney.com.au/`,
+                description: `Chef Matt Moran presents Modern Australian haute cuisine in an elegant, glass-walled dining room.`,
+                img: `https://media-cdn.tripadvisor.com/media/photo-s/18/01/b6/1c/aria-sydney.jpg`,
+                type: [
+                    `Fine dining`,
+                    `Modern Australian haute cuisine`
+                ],
+                rating: 4.5
+            },
+            {
+                name: `Restaurant Hubert`,
+                address: `15 Bligh St, Sydney NSW 2000, Australia`,
+                website: `https://www.swillhouse.com/`,
+                description: `Classic French dishes showcased in a sophisticated venue featuring a lively bar & vintage decor.`,
+                img: `https://media.cntraveler.com/photos/59c946a67448196e0e771a95/16:9/w_2560,c_limit/cr-hubert.jpg`,
+                type: [
+                    `French Restaurant`,
+                    `Vintage`
+                ],
+                rating: 4.6
+            },
+            {
+                name: `Long Chim Sydney`,
+                address: `Corner of Pitt St and Angel Place, Angel Pl, Sydney NSW 2000, Australia`,
+                website: `https://www.longchimsydney.com/`,
+                description: `Hip eatery in an industrial space serving Thai classics including curries, stir fries & meat dishes.`,
+                img: `https://images.squarespace-cdn.com/content/v1/5ed465331aadaa1625c491f7/1591177799066-V4BWSQA1B036PXSMEKNP/long+chim+interior.jpg?format=2500w`,
+                type: [
+                    `Thai Restaurant`,
+                    `Hip`
+                ],
+                rating: 4.1
+            },
+            {
+                name: `Chophouse Sydney`,
+                address: `25 Bligh St, Sydney NSW 2000, Australia`,
+                website: `https://www.chophousesydney.com.au/`,
+                description: `A high-end, contemporary steakhouse dishing up premium meats in a warmly lit vintage space.`,
+                img: `https://youimg1.tripcdn.com/target/100c0f0000007ddn49416.jpg`,
+                type: [
+                    `Steakhouse`,
+                    `Vintage`
+                ],
+                rating: 4.3
+            },
+            {
+                name: `Bistro Papillon`,
+                address: `98 Clarence St, Sydney NSW 2000, Australia`,
+                website: `https://www.bistropapillon.com.au/`,
+                description: `Onion soup, coq au vin and other French dishes in a cosy, romantic space decorated with old posters.`,
+                img: `https://i0.wp.com/www.bistropapillon.com.au/wp-content/uploads/2020/05/PrivateParty__0005_A42I2569.jpg?resize=1000%2C667&ssl=1`,
+                type: [
+                    `French Restaurant`,
+                    `Romantic`
+                ],
+                rating: 4.5
+            },
+            {
+                name: `NOMAD Sydney`,
+                address: `16 Foster St, Surry Hills NSW 2010, Australia`,
+                website: `https://nomad.sydney/`,
+                description: `Upbeat restaurant in a converted warehouse serving contemporary cuisine with Middle Eastern accents.`,
+                img: `https://media.timeout.com/images/105917669/750/422/image.jpg`,
+                type: [
+                    `Australian Restaurant`,
+                    `Converted Warehouse`
+                ],
+                rating: 4.6
             }
         ]
     },
     {
-        location: `LA`,
+        city: `London`,
         restaurants: [
             {
-                name: `asdf`
+                name: `Twist Connubio`,
+                address: `42 Crawford St, London W1H 1JW, United Kingdom`,
+                website: `https://www.twistconnubio.com/`,
+                description: `Rustic-chic dining room serving international, ingredient-led small plates cooked on a Josper grill.`,
+                img: `https://www.hardens.com/restaurant-images/UNADJUSTEDNONRAW_thumb_1524/RG-17fd-8571-mp.jpg`,
+                type: [
+                    `Small plate`,
+                    `Rustic-chic`
+                ],
+                rating: 4.6
+            },
+            {
+                name: `Berners Tavern`,
+                address: `10 Berners St, London W1T 3NP, United Kingdom`,
+                website: `http://www.bernerstavern.com/`,
+                description: `Jason Atherton's upmarket Modern British restaurant in Ian Schrager's London Edition Hotel.`,
+                img: ``,
+                type: [
+                    `European Restaurant`,
+                    `Modern`
+                ],
+                rating: 4.5
+            },
+            {
+                name: `Aquavit London`,
+                address: `1 St James's Market, St. James's, London Sw1Y 4QQ, United Kingdom`,
+                website: `https://www.aquavitrestaurants.com/`,
+                description: `Polished eatery spotlighting elevated Scandinavian cuisine & desserts, plus a robust wine list.`,
+                img: `https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/34/2017/05/Aquavit-restaurant-London-interior.jpg`,
+                type: [
+                    `Scandinavian Restaurant`,
+                    `Robust wine list`
+                ],
+                rating: 4.4
+            },
+            {
+                name: `Jikoni`,
+                address: `19-21 Blandford St, London W1U 3DJ, United Kingdom`,
+                website: `https://www.jikonilondon.com/`,
+                description: `Cosy kitchen-style diner serving Asian comfort food with British, African and Middle Eastern twists.`,
+                img: `https://resizer.otstatic.com/v2/photos/wide-large/1/26030092.jpg`,
+                type: [
+                    `Asian fusion restaurant`,
+                    `British, African and Middle Eastern twists`
+                ],
+                rating: 4.5
+            },
+            {
+                name: `Launceston Place`,
+                address: `1A Launceston Pl, London W8 5RL, United Kingdom`,
+                website: `https://www.launcestonplace-restaurant.co.uk/`,
+                description: `Fine dining restaurant in an 1839 townhouse, with a notable wine list and Modern British menu.`,
+                img: `https://resizer.otstatic.com/v2/photos/wide-xlarge/1/25542340.jpg`,
+                type: [
+                    `Modern European Restaurant`,
+                    `Fine dining`
+                ],
+                rating: 4.8
+            },
+            {
+                name: `Lyle's`,
+                address: `Tea Building, 56 Shoreditch High St, London E1 6GY, United Kingdom`,
+                website: `https://www.lyleslondon.com/`,
+                description: `This stripped back dining room serves seasonal British fare as a la carte lunches and set suppers.`,
+                img: `https://www.theworlds50best.com/filestore/jpg/W50BR2021-150-Lyle's-1.jpg`,
+                type: [
+                    `European Restaurant`,
+                    `Modern/Industrial`
+                ],
+                rating: 4.5
+            }
+        ]
+    },
+    {
+        city: `Dubai`,
+        restaurants: [
+            {
+                name: `Miss Tess`,
+                address: `Taj Dubai - Burj Khalifa St - Business Bay - Dubai - United Arab Emirates`,
+                website: `https://www.misstessdubai.com/`,
+                description: `Asian Street Food, daily shows, and delicious cocktails! Miss Tess is an Asian street-food restaurant inspired by a story.`,
+                img: `https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/12/XumtkjIw-Miss-Tess-1200x800.jpg`,
+                type: [
+                    `Asian street-food`,
+                    `Experience`
+                ],
+                rating: 4.3
+            },
+            {
+                name: `Günaydin`,
+                address: `Souk Al Bahar - Downtown Dubai - Dubai - United Arab Emirates`,
+                website: `https://www.gunaydindubai.com/`,
+                description: `Set in the beautiful Souk Al Bahar development, Günaydin Meatery Dubai sits on the waterfront promenade, enchanting guests with a spectacular views of the world-famous Dubai Fountain and the Burj Khalifa.`,
+                img: `https://b.zmtcdn.com/data/pictures/3/18410863/2cdabe1ef0826c86888d90156bd6d502.jpg`,
+                type: [
+                    `Turkish Restaurant`,
+                    `High quality meats`
+                ],
+                rating: 4.2
+            },
+            {
+                name: `Tribes Dubai Mall`,
+                address: `Level 2, Star Atrium, above apple store, The Dubai Mall - Dubai - United Arab Emirates`,
+                website: `https://www.tribesrestaurant.com/`,
+                description: `Traditional & fusion dishes from African countries highlighted in a stylish venue with a terrace.`,
+                img: `https://www.ramadigroup.com/wp-content/uploads/75efabd8a2075f0cbf30e5f2ff052bc9.jpg`,
+                type: [
+                    `African Restaurant`,
+                    `Fushion`
+                ],
+                rating: 4.5
+            },
+            {
+                name: `Tulum Restaurant`,
+                address: `The Dubai Mall Unit No. 4F - 04, Fashion Avenue Extention - Dubai - United Arab Emirates`,
+                website: `https://tulum.ae/`,
+                description: `Classic Mexican cooking with a twist is served at this elevated eatery that has stylish decor.`,
+                img: `https://media-cdn.tripadvisor.com/media/photo-s/19/c6/23/8e/caption.jpg`,
+                type: [
+                    `Mexican Restaurant`,
+                    `Experience`
+                ],
+                rating: 4.2
+            },
+            {
+                name: `Armani/Ristorante`,
+                address: `Burj Khalifa - Lobby Level, Armani Hotel Dubai - Sheikh Mohammed bin Rashid Blvd - Downtown Dubai - Dubai - United Arab Emirates`,
+                website: `https://www.armanihotels.com/en/restaurant/armani-ristorante/?hotel=armani-hotel-dubai`,
+                description: `Contemporary Italian restaurant in hotel with chic decor, a bar, tasting menu and city views.`,
+                img: `https://resizer.otstatic.com/v2/photos/xlarge/1/24885358.jpg`,
+                type: [
+                    `Italian Restaurant`,
+                    `Contemporary`
+                ],
+                rating: 4.4
+            },
+            {
+                name: `11 Woodfire`,
+                address: `Villa 11 75B St - Jumeirah - Jumeirah 1 - Dubai - United Arab Emirates`,
+                website: `https://11woodfire.com/?utm_source=GoogleBusinessProfiles&utm_medium=Website&utm_campaign=MapLabs`,
+                description: `Produce and ingredients that are carefully selected globally and locally from premium and responsible vendors.`,
+                img: `https://www.timeoutdubai.com/cloud/timeoutdubai/2022/08/11/11-Woodfire-1024x768.jpg`,
+                type: [
+                    `Fine dining`,
+                    `Steakhouse`
+                ],
+                rating: 4.2
             }
         ]
     }
